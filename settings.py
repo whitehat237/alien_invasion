4e7058191e238028f812e9fcec21c96f87de6605
Chris Smith <christopher.e.smith54@gmail.com>
2016-11-23 00:26:47 -0600

    Fixed indentation issue with function call to initialize_dynamic_settings() in settings.py

--- settings.py
+++ settings.py
@@ -10,18 +10,15 @@ class Settings():
         self.bg_color = (230, 230, 230)
 
         # Ship settings
-        self.ship_speed_factor = 1.5
         self.ship_limit = 3
         
         # Bullet settings
-        self.bullet_speed_factor = 3
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 4
 
         # Alien settings
-        self.alien_speed_factor = 1
         self.fleet_drop_speed = 10
         
         # How quickly the game speeds up.
@@ -30,11 +27,8 @@ class Settings():
         # Initialize dynamic settings
         self.initialize_dynamic_settings()
 
-        # fleet direction of 1 represents right; -1 represents left.
-        self.fleet_direction = 1
 
-
-def initialize_dynamic_settings(self):
+    def initialize_dynamic_settings(self):
         """Initialize settings that change throughout the game."""
         
         self.ship_speed_factor = 1.5
