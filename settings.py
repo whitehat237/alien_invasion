473ffbc51c233508c3f8795c93d1a7893d57f508
Chris Smith <christopher.e.smith54@gmail.com>
2016-11-23 00:02:43 -0600

    Added global speed factor, created new function to initialize and store dynamic event data while the game is running.

--- settings.py
+++ settings.py
@@ -2,7 +2,8 @@ class Settings():
     """ A class to store all settings for Alien Invasion."""
     
     def __init__(self):
-        """Initialize the game's settings."""
+        """Initialize the game's static settings."""
+
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 700
@@ -22,7 +23,32 @@ class Settings():
         # Alien settings
         self.alien_speed_factor = 1
         self.fleet_drop_speed = 10
+        
+        # How quickly the game speeds up.
+        self.speedup_scale = 1.1
+
+        # Initialize dynamic settings
+        self.initialize_dynamic_settings()
+
         # fleet direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
 
 
+def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game."""
+        
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
+        self.alien_speed_factor = 1
+
+        # fleet direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
+
+
+def increase_speed(self):
+        """Increase speed settings."""
+        
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
+        
