3a8f6de73e55ed281f41934c86e0ea8bffa2ed00
Chris Smith <christopher.e.smith54@gmail.com>
2016-11-23 00:11:43 -0600

    Reset settings back to default values when the game ends

--- game_functions.py
+++ game_functions.py
@@ -64,8 +64,9 @@ def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
     if len(aliens) == 0:
-      # Destroy existing bullets and create new fleet.
+      # Destroy existing bullets, speed-up the game, and create new fleet.
       bullets.empty()
+      ai_settings.increase_speed()
       create_fleet(ai_settings, screen, ship, aliens)
 
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
@@ -123,6 +124,9 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bul
     
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
+
+        # Reset the game settings back to default values
+        ai_settings.initialize_dynamic_settings()
 
         # Hide the mouse cursor
         pygame.mouse.set_visible(False)
